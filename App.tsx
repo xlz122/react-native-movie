61f0b940d3088d83bc9136e30917eba34a3656e6
xlz122 <122874722@qq.com>
2022-08-29 09:43:34 +0800

    chore: 删除忽略react-native-gesture-handler警告代码

--- App.tsx
+++ App.tsx
@@ -4,12 +4,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './src/router/StackNavigator';
 import { Provider } from 'react-redux';
 import store from './src/store/index';
-// 忽略react-native-gesture-handler警告
-// import { LogBox } from 'react-native';
-
-// LogBox.ignoreLogs([
-//   "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
-// ]);
 
 function App(): React.ReactElement {
   return (
