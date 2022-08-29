47a3ce3f1e1a96e7ae804d00e2c47b220aac55c9
xlz122 <122874722@qq.com>
2022-08-29 17:16:21 +0800

    chore: 完成首页搜索、轮播图

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,8 +1,48 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { index } from '../../api/home';
+import Search from './search/Search';
+import Banner from './banner/Banner';
 
 function Home(): React.ReactElement {
-  return <Text>Home</Text>;
+  // 轮播图
+  const [banner, setBanner] = useState([]);
+
+  const getIndex = () => {
+    index()
+      .then((res: any) => {
+        if (res.code === 200) {
+          setBanner(res.data.swiper);
+        }
+      })
+      .catch(() => ({}));
+  };
+
+  useEffect(() => {
+    getIndex();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.bgColor} />
+      <Search />
+      <Banner banner={banner} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f5f5f5'
+  },
+  bgColor: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: -1,
+    width: '100%',
+    height: 300
+  }
+});
 
 export default Home;
