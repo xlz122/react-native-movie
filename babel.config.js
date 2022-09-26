9066cec4acb4481ce8b406c5f057532bc517f903
xlz122 <122874722@qq.com>
2022-09-26 14:25:49 +0800

    chore: modify babel config

--- babel.config.js
+++ babel.config.js
@@ -8,14 +8,7 @@ module.exports = {
         root: ['./src'],
         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
         alias: {
-          '@': ['./src'],
-          '@api': './src/api',
-          '@assets': './src/assets',
-          '@components': './src/components',
-          '@router': './src/router ',
-          '@store': './src/store ',
-          '@utils': './src/utils ',
-          '@views': './src/views '
+          '@': ['./src']
         }
       }
     ]
