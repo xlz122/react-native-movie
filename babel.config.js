4728222e5aba3d067a09c8ba8d502fd6fc18e841
xlz122 <122874722@qq.com>
2022-09-26 11:43:09 +0800

    chore: 添加babel-plugin-module-resolver插件配置路径别名

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,24 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin', 'react-native-web']
+  plugins: [
+    'react-native-reanimated/plugin',
+    'react-native-web',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': ['./src'],
+          '@api': './src/api',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@router': './src/router ',
+          '@store': './src/store ',
+          '@utils': './src/utils ',
+          '@views': './src/views '
+        }
+      }
+    ]
+  ]
 };
