c4d641372d9a6285a1ad64c225d1344f43404951
xlz122 <122874722@qq.com>
2022-09-27 10:43:38 +0800

    feat: 我的-设置按钮登录后才显示

--- src/tabpage/mine/Mine.tsx
+++ src/tabpage/mine/Mine.tsx
@@ -18,6 +18,8 @@ function Mine(props: Props): React.ReactElement {
   const userinfo = useSelector(
     (state: RootState) => state.routine.userinfo
   ) as UserInfo;
+  const isLogin = useSelector((state: RootState) => state.routine.isLogin);
+  console.log(isLogin);
 
   return (
     <View style={styles.page}>
@@ -47,13 +49,15 @@ function Mine(props: Props): React.ReactElement {
             </Text>
           </>
         )}
-        <TouchableOpacity
-          activeOpacity={1}
-          onPress={() => props?.navigation.push('Setting')}
-          style={styles.setting}
-        >
-          <Text style={styles.settingIcon}>{'\ue65e'}</Text>
-        </TouchableOpacity>
+        {Boolean(isLogin) && (
+          <TouchableOpacity
+            activeOpacity={1}
+            onPress={() => props?.navigation.push('Setting')}
+            style={styles.setting}
+          >
+            <Text style={styles.settingIcon}>{'\ue65e'}</Text>
+          </TouchableOpacity>
+        )}
       </View>
       <View style={styles.menu}>
         <View style={styles.menuItem}>
