1be97ea3601018932555d105e85d9e7f1c4646a3
xlz122 <122874722@qq.com>
2022-11-03 14:02:34 +0800

    fix: 修复影视列表显示评分0的bug

--- src/views/search/search-detail/SearchDetail.tsx
+++ src/views/search/search-detail/SearchDetail.tsx
@@ -115,9 +115,11 @@ function SearchDetail(props: Props): React.ReactElement {
             {item.countries}
           </Text>
         </View>
-        <Text style={styles.itemRating}>
-          <Text style={styles.itemRatingWeight}>{item?.rating}</Text>分
-        </Text>
+        {Number(item?.rating) > 0 && (
+          <Text style={styles.itemRating}>
+            <Text style={styles.itemRatingWeight}>{item?.rating}</Text> 分
+          </Text>
+        )}
       </View>
     </TouchableOpacity>
   );
